11633983fb03ae3ccc428194891890cd50db22f1
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2022-10-31 18:49:20 +0700

    fix ui login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 // import { Button } from "antd";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
-import Register from "./pages/Register";
 import Home from "./pages/Home";
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
         </Routes>
       </BrowserRouter>
     </div>
