530df9248f918f553fd4748cf5a9ce3380ae133d
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2022-10-31 09:39:17 +0700

    fix bug desain

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import "./App.css";
+import { Button } from "antd";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App m-5">
       <h1>Fist code</h1>
+      <Button type="primary">Button</Button>
     </div>
   );
 }
