eca1fa4df1a84c04f06204d9f0cefb99ebecf093
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2022-11-08 15:45:39 +0700

    fix no error

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,6 +1,7 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { message } from "antd";
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { HideLoading, ShowLoading } from "../redux/alertsSlice";
