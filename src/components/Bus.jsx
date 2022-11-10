762171c97cda4b327bfc9c1d40fa5338875134af
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2022-11-10 09:19:56 +0700

    fix create bus card desain

--- src/components/Bus.jsx
+++ src/components/Bus.jsx
@@ -1,9 +1,35 @@
 import React from "react";
 
-const Bus = () => {
+const Bus = ({ bus }) => {
   return (
-    <div>
-      <h1>This A Card Bus</h1>
+    <div className="card p-2">
+      <h1 className="text-lg primary-text">{bus.name}</h1>
+      <hr />
+      <div className="d-flex justify-content-between">
+        <div>
+          <p className="text-sm">From</p>
+          <p className="text-sm">{bus.from}</p>
+        </div>
+
+        <div>
+          <p className="text-sm">To</p>
+          <p className="text-sm">{bus.to}</p>
+        </div>
+
+        <div>
+          <p className="text-sm">Fare</p>
+          <p className="text-sm">$ {bus.fare} /-</p>
+        </div>
+      </div>
+      <hr />
+      <div className="d-flex justify-content-between align-items-end">
+        <div>
+          <p className="text-sm">Joureny Date</p>
+          <p className="text-sm">{bus.journeyDate}</p>
+        </div>
+
+        <h1 className="text-lg underline secondary-text">Book Now</h1>
+      </div>
     </div>
   );
 };
