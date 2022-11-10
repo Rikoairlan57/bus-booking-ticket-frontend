50846412ca25e5cfdaabf7f2a0d2b236bbb0e16e
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2022-11-10 09:08:51 +0700

    Card Bus Stater Pack

---
+++ src/components/Bus.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Bus = () => {
+  return (
+    <div>
+      <h1>This A Card Bus</h1>
+    </div>
+  );
+};
+
+export default Bus;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,42 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { Col, Row, message } from "antd";
+import { axiosInstance } from "../helpers/axiosInstance";
+import { HideLoading, ShowLoading } from "../redux/alertsSlice";
+import { useDispatch } from "react-redux";
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const [buses, setBuses] = useState([]);
+
+  const getBuses = async () => {
+    try {
+      dispatch(ShowLoading());
+      const response = await axiosInstance.post("/api/buses/get-all-buses", {});
+      dispatch(HideLoading());
+      if (response.data.success) {
+        setBuses(response.data.data);
+      } else {
+        message.error(response.data.message);
+      }
+    } catch (error) {
+      dispatch(HideLoading());
+      message.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getBuses();
+  }, []);
   return (
     <div>
-      <h1>HomePage</h1>
+      <div>HOMEPAGE</div>
+      <div>
+        <Row>
+          {buses.map((bus) => (
+            <Col></Col>
+          ))}
+        </Row>
+      </div>
     </div>
   );
 };
